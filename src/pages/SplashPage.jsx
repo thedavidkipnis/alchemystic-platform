9bb71dc095e0f7e6da281f7562bdc5613c3e0644
David Kipnis <thedavidkipnis@gmail.com>
2025-08-04 10:50:31 -0700

    Added scrollables section

--- src/pages/SplashPage.jsx
+++ src/pages/SplashPage.jsx
@@ -6,12 +6,23 @@ export default function SplashPage() {
   return (
     <div className="splash-container">
       <Navbar isLogInButtonVisible={true}/>
+      <div className="welcome-banner">
+        
 
-      <div className="splash-text">
-        <p>
-          Discover an innovative approach to healing with ketamine that is
-          transformative, responsible, <br /> and deeply human.
-        </p>
+        <div className="splash-text">
+          <p>
+            Discover an innovative approach to healing with ketamine that is
+            transformative, responsible, <br /> and deeply human.
+          </p>
+        </div>
+      </div>
+
+      <div className="collapsables-banner">
+        <div className="splash-text">
+          <p>
+            Learn more about our unique protocols and resources designed to support healing.
+          </p>
+        </div>
       </div>
     </div>
   );
